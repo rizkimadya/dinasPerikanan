96929c38e950ec6a88223e4b82b3d348cbfe09db
rizkimadya <rizkimadya88@gmail.com>
2023-11-14 04:11:53 +0800

    perbaiikan icon

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,17 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AlbumKegiatan;
 use App\Models\Bantuan;
+use App\Models\Berita;
 use App\Models\Blog;
 use App\Models\Komentar;
+use App\Models\Pengaduan;
+use App\Models\Pengumuman;
 use App\Models\Produk;
+use App\Models\Survei;
 use App\Models\Testimoni;
+use App\Models\VideoKegiatan;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view('Admin.dashboard.index');
+        $berita = Berita::count();
+        $album = AlbumKegiatan::count();
+        $video = VideoKegiatan::count();
+        $pengumuman = Pengumuman::count();
+        $pengaduan = Pengaduan::count();
+        $survei = Survei::count();
+
+        return view('Admin.dashboard.index', compact('berita', 'album', 'video', 'pengumuman', 'pengaduan', 'survei'));
     }
 }
--- resources/views/Admin/dashboard/index.blade.php
+++ resources/views/Admin/dashboard/index.blade.php
@@ -5,107 +5,79 @@
         <div class="col-md-4">
             <div class="card p-md-5 p-4 mb-md-5 mb-3">
                 <div class="d-flex align-items-center">
-                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">100</span>
+                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{ $berita }}</span>
                     <span class="menu-icon ms-auto">
                         <!--begin::Svg Icon | path: icons/duotune/general/gen002.svg-->
                         <span class="svg-icon svg-icon-3 text-white">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
-                                <path opacity="0.3"
-                                    d="M18.041 22.041C18.5932 22.041 19.041 21.5932 19.041 21.041C19.041 20.4887 18.5932 20.041 18.041 20.041C17.4887 20.041 17.041 20.4887 17.041 21.041C17.041 21.5932 17.4887 22.041 18.041 22.041Z"
-                                    fill="currentColor" />
-                                <path opacity="0.3"
-                                    d="M6.04095 22.041C6.59324 22.041 7.04095 21.5932 7.04095 21.041C7.04095 20.4887 6.59324 20.041 6.04095 20.041C5.48867 20.041 5.04095 20.4887 5.04095 21.041C5.04095 21.5932 5.48867 22.041 6.04095 22.041Z"
-                                    fill="currentColor" />
-                                <path opacity="0.3"
-                                    d="M7.04095 16.041L19.1409 15.1409C19.7409 15.1409 20.141 14.7409 20.341 14.1409L21.7409 8.34094C21.9409 7.64094 21.4409 7.04095 20.7409 7.04095H5.44095L7.04095 16.041Z"
-                                    fill="currentColor" />
                                 <path
-                                    d="M19.041 20.041H5.04096C4.74096 20.041 4.34095 19.841 4.14095 19.541C3.94095 19.241 3.94095 18.841 4.14095 18.541L6.04096 14.841L4.14095 4.64095L2.54096 3.84096C2.04096 3.64096 1.84095 3.04097 2.14095 2.54097C2.34095 2.04097 2.94096 1.84095 3.44096 2.14095L5.44096 3.14095C5.74096 3.24095 5.94096 3.54096 5.94096 3.84096L7.94096 14.841C7.94096 15.041 7.94095 15.241 7.84095 15.441L6.54096 18.041H19.041C19.641 18.041 20.041 18.441 20.041 19.041C20.041 19.641 19.641 20.041 19.041 20.041Z"
+                                    d="M20 7H3C2.4 7 2 6.6 2 6V3C2 2.4 2.4 2 3 2H20C20.6 2 21 2.4 21 3V6C21 6.6 20.6 7 20 7ZM7 9H3C2.4 9 2 9.4 2 10V20C2 20.6 2.4 21 3 21H7C7.6 21 8 20.6 8 20V10C8 9.4 7.6 9 7 9Z"
+                                    fill="currentColor" />
+                                <path opacity="0.3"
+                                    d="M20 21H11C10.4 21 10 20.6 10 20V10C10 9.4 10.4 9 11 9H20C20.6 9 21 9.4 21 10V20C21 20.6 20.6 21 20 21Z"
                                     fill="currentColor" />
                             </svg>
                         </span>
                         <!--end::Svg Icon-->
                     </span>
                 </div>
-                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Produk</span>
+                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Berita</span>
             </div>
         </div>
         <div class="col-md-4">
             <div class="card p-md-5 p-4 mb-md-5 mb-3">
                 <div class="d-flex align-items-center">
-                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">100</span>
+                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{ $album }}</span>
                     <span class="menu-icon ms-auto">
                         <!--begin::Svg Icon | path: icons/duotune/general/gen002.svg-->
-                        <span class="svg-icon svg-icon-2">
+                        <span class="svg-icon svg-icon-3 text-white">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path
-                                    d="M20 19.725V18.725C20 18.125 19.6 17.725 19 17.725H5C4.4 17.725 4 18.125 4 18.725V19.725H3C2.4 19.725 2 20.125 2 20.725V21.725H22V20.725C22 20.125 21.6 19.725 21 19.725H20Z"
+                                    d="M11.7 8L7.49998 15.3L4.59999 20.3C3.49999 18.4 3.1 17.7 2.3 16.3C1.9 15.7 1.9 14.9 2.3 14.3L8.8 3L11.7 8Z"
                                     fill="currentColor" />
                                 <path opacity="0.3"
-                                    d="M22 6.725V7.725C22 8.325 21.6 8.725 21 8.725H18C18.6 8.725 19 9.125 19 9.725C19 10.325 18.6 10.725 18 10.725V15.725C18.6 15.725 19 16.125 19 16.725V17.725H15V16.725C15 16.125 15.4 15.725 16 15.725V10.725C15.4 10.725 15 10.325 15 9.725C15 9.125 15.4 8.725 16 8.725H13C13.6 8.725 14 9.125 14 9.725C14 10.325 13.6 10.725 13 10.725V15.725C13.6 15.725 14 16.125 14 16.725V17.725H10V16.725C10 16.125 10.4 15.725 11 15.725V10.725C10.4 10.725 10 10.325 10 9.725C10 9.125 10.4 8.725 11 8.725H8C8.6 8.725 9 9.125 9 9.725C9 10.325 8.6 10.725 8 10.725V15.725C8.6 15.725 9 16.125 9 16.725V17.725H5V16.725C5 16.125 5.4 15.725 6 15.725V10.725C5.4 10.725 5 10.325 5 9.725C5 9.125 5.4 8.725 6 8.725H3C2.4 8.725 2 8.325 2 7.725V6.725L11 2.225C11.6 1.925 12.4 1.925 13.1 2.225L22 6.725ZM12 3.725C11.2 3.725 10.5 4.425 10.5 5.225C10.5 6.025 11.2 6.725 12 6.725C12.8 6.725 13.5 6.025 13.5 5.225C13.5 4.425 12.8 3.725 12 3.725Z"
+                                    d="M11.7 8L8.79999 3H13.4C14.1 3 14.8 3.4 15.2 4L20.6 13.3H14.8L11.7 8ZM7.49997 15.2L4.59998 20.2H17.6C18.3 20.2 19 19.8 19.4 19.2C20.2 17.7 20.6 17 21.7 15.2H7.49997Z"
                                     fill="currentColor" />
                             </svg>
                         </span>
                         <!--end::Svg Icon-->
                     </span>
                 </div>
-                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Blog</span>
+                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Album Kegiatan</span>
             </div>
         </div>
         <div class="col-md-4">
             <div class="card p-md-5 p-4 mb-md-5 mb-3">
                 <div class="d-flex align-items-center">
-                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">100</span>
+                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{ $video }}</span>
                     <span class="menu-icon ms-auto">
                         <!--begin::Svg Icon | path: icons/duotune/general/gen002.svg-->
-                        <span class="svg-icon svg-icon-2">
+                        <span class="svg-icon svg-icon-3 text-white">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
-                                <path opacity="0.3"
-                                    d="M21 19H3C2.4 19 2 18.6 2 18V6C2 5.4 2.4 5 3 5H21C21.6 5 22 5.4 22 6V18C22 18.6 21.6 19 21 19Z"
-                                    fill="currentColor" />
                                 <path
-                                    d="M21 5H2.99999C2.69999 5 2.49999 5.10005 2.29999 5.30005L11.2 13.3C11.7 13.7 12.4 13.7 12.8 13.3L21.7 5.30005C21.5 5.10005 21.3 5 21 5Z"
+                                    d="M11.7 8L7.49998 15.3L4.59999 20.3C3.49999 18.4 3.1 17.7 2.3 16.3C1.9 15.7 1.9 14.9 2.3 14.3L8.8 3L11.7 8Z"
                                     fill="currentColor" />
-                            </svg>
-                        </span>
-                        <!--end::Svg Icon-->
-                    </span>
-                </div>
-                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Testimoni</span>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card p-md-5 p-4 mb-md-5 mb-3">
-                <div class="d-flex align-items-center">
-                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">100</span>
-                    <span class="menu-icon ms-auto">
-                        <!--begin::Svg Icon | path: icons/duotune/general/gen002.svg-->
-                        <span class="svg-icon svg-icon-2">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
                                 <path opacity="0.3"
-                                    d="M20 3H4C2.89543 3 2 3.89543 2 5V16C2 17.1046 2.89543 18 4 18H4.5C5.05228 18 5.5 18.4477 5.5 19V21.5052C5.5 22.1441 6.21212 22.5253 6.74376 22.1708L11.4885 19.0077C12.4741 18.3506 13.6321 18 14.8167 18H20C21.1046 18 22 17.1046 22 16V5C22 3.89543 21.1046 3 20 3Z"
+                                    d="M11.7 8L8.79999 3H13.4C14.1 3 14.8 3.4 15.2 4L20.6 13.3H14.8L11.7 8ZM7.49997 15.2L4.59998 20.2H17.6C18.3 20.2 19 19.8 19.4 19.2C20.2 17.7 20.6 17 21.7 15.2H7.49997Z"
                                     fill="currentColor" />
-                                <rect x="6" y="12" width="7" height="2" rx="1" fill="currentColor" />
-                                <rect x="6" y="7" width="12" height="2" rx="1" fill="currentColor" />
                             </svg>
                         </span>
                         <!--end::Svg Icon-->
                     </span>
                 </div>
-                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Komentar</span>
+                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Video</span>
             </div>
         </div>
         <div class="col-md-4">
             <div class="card p-md-5 p-4 mb-md-5 mb-3">
                 <div class="d-flex align-items-center">
-                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">100</span>
+                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{ $pengumuman }}</span>
                     <span class="menu-icon ms-auto">
                         <!--begin::Svg Icon | path: icons/duotune/general/gen002.svg-->
-                        <span class="svg-icon svg-icon-3">
+                        <span class="svg-icon svg-icon-3 text-white">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path
@@ -119,7 +91,51 @@
                         <!--end::Svg Icon-->
                     </span>
                 </div>
-                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Bantuan</span>
+                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Pengumuman</span>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-md-5 p-4 mb-md-5 mb-3">
+                <div class="d-flex align-items-center">
+                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{ $pengaduan }}</span>
+                    <span class="menu-icon ms-auto">
+                        <!--begin::Svg Icon | path: icons/duotune/general/gen002.svg-->
+                        <span class="svg-icon svg-icon-3 text-white">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.3"
+                                    d="M20 3H4C2.89543 3 2 3.89543 2 5V16C2 17.1046 2.89543 18 4 18H4.5C5.05228 18 5.5 18.4477 5.5 19V21.5052C5.5 22.1441 6.21212 22.5253 6.74376 22.1708L11.4885 19.0077C12.4741 18.3506 13.6321 18 14.8167 18H20C21.1046 18 22 17.1046 22 16V5C22 3.89543 21.1046 3 20 3Z"
+                                    fill="currentColor" />
+                                <rect x="6" y="12" width="7" height="2" rx="1" fill="currentColor" />
+                                <rect x="6" y="7" width="12" height="2" rx="1" fill="currentColor" />
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                    </span>
+                </div>
+                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Pengaduan</span>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card p-md-5 p-4 mb-md-5 mb-3">
+                <div class="d-flex align-items-center">
+                    <span class="fs-2hx fw-bold text-dark me-2 lh-1 ls-n2">{{ $survei }}</span>
+                    <span class="menu-icon ms-auto">
+                        <!--begin::Svg Icon | path: icons/duotune/general/gen002.svg-->
+                        <span class="svg-icon svg-icon-3 text-white">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.3"
+                                    d="M20 3H4C2.89543 3 2 3.89543 2 5V16C2 17.1046 2.89543 18 4 18H4.5C5.05228 18 5.5 18.4477 5.5 19V21.5052C5.5 22.1441 6.21212 22.5253 6.74376 22.1708L11.4885 19.0077C12.4741 18.3506 13.6321 18 14.8167 18H20C21.1046 18 22 17.1046 22 16V5C22 3.89543 21.1046 3 20 3Z"
+                                    fill="currentColor" />
+                                <rect x="6" y="12" width="7" height="2" rx="1" fill="currentColor" />
+                                <rect x="6" y="7" width="12" height="2" rx="1" fill="currentColor" />
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->
+                    </span>
+                </div>
+                <span class="text-gray-400 pt-1 fw-semibold fs-6">Jumlah Survei Kepuasan</span>
             </div>
         </div>
     </div>
