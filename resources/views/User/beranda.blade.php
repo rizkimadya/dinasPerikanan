57c70a69f13822f638206b622d5aa62479a3cf08
rizki madya <rizkimadya88@gmail.com>
2024-08-26 02:47:22 +0800

    perbaikan

--- resources/views/User/beranda.blade.php
+++ resources/views/User/beranda.blade.php
@@ -224,7 +224,7 @@
 
             <div class="row">
 
-                <div class="col-lg-3 col-md-6">
+                <div class="col-lg-4 col-md-6">
                     <div class="count-box">
                         <i class="bi bi-journal-richtext"></i>
                         <span data-purecounter-start="0" data-purecounter-end="{{ $jumpengaduan }}"
@@ -233,7 +233,7 @@
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-md-6 mt-5 mt-md-0">
+                <div class="col-lg-4 col-md-6 mt-5 mt-md-0">
                     <div class="count-box">
                         <i class="bi bi-emoji-smile"></i>
                         <span data-purecounter-start="0" data-purecounter-end="{{ $jumsurvei }}"
@@ -242,7 +242,7 @@
                     </div>
                 </div>
 
-                <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
+                <div class="col-lg-4 col-md-6 mt-5 mt-lg-0">
                     <div class="count-box">
                         <i class="bi bi-book"></i>
                         <span data-purecounter-start="0" data-purecounter-end="{{ $jumberita }}"
@@ -250,7 +250,7 @@
                         <p>Jumlah Berita</p>
                     </div>
                 </div>
-
+{{--
                 <div class="col-lg-3 col-md-6 mt-5 mt-lg-0">
                     <div class="count-box">
                         <i class="bi bi-headset"></i>
@@ -258,7 +258,7 @@
                             data-purecounter-duration="1" class="purecounter"></span>
                         <p>Jumlah Pengumuman</p>
                     </div>
-                </div>
+                </div> --}}
 
             </div>
 
